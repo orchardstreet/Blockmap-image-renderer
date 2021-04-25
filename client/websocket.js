8c601e87be394e825c7160a8ea99005b5600d109
orchardstreet <39713147+orchardstreet@users.noreply.github.com>
2021-04-25 05:24:19 -0700

    Update websocket.js

--- client/websocket.js
+++ client/websocket.js
@@ -10,18 +10,11 @@ wss.onopen = () => {
 };
 
 wss.addEventListener("message", e => {
-//    document.getElementById("response").innerHTML = JSON.parse(e.data);
 	var responsee = JSON.parse(e.data);
-//	console.log(e.data);
-//	console.log(typeof responsee);
-//	console.log(responsee);
-//	console.log(responsee["response"]);
 	if (responsee["response"] == "error") {
 	document.getElementById("response").innerHTML = responsee["value"];
         setTimeout(function() {document.getElementById("response").innerHTML = "";  },3000);
 	} else if (responsee["response"] == "singleRawImageData") {
-//	document.getElementById("response").innerHTML = JSON.stringify(responsee);      
-		
 		
 		//PLUG IN VALUES HEEEERRREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!
 	renderBlockmap(responsee["colorIndex"],responsee["pixelGroups"],responsee["transparentPixelGroups"],responsee["transparentPixelGroupIndexes"],responsee["pixelGroupIndexes"],responsee["pixelData"]);
